e5c90b1be4f53db0f622079e5a7d5c8ba0b93e81
Pop2323 <omar4500455@gmail.com>
2023-08-20 11:08:19 -0700

    My first commit

---
+++ getline.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * _getline - func that get the number of the input
+ *
+ * Return: return ptr of buffer
+*/
+char *_getline()
+{
+	char ch;
+	char  *input;
+	size_t buf_size = 1024, i = 0;
+	static  char  *buffer, *copy;
+
+	buffer  = malloc(sizeof(char) * buf_size);
+
+	if (buffer == NULL)
+		return (NULL);
+	_memset(buffer, buf_size);
+	input = buffer;
+	ch = getchar
+
+		if (ch == -1)
+		{
+			buffer[0] = 0x04;
+			return (buffer);
+			while (!(ch == EOF || ch == '\n'))
+				if (i > buf_size)
+				{
+					buf_size = buf_size * 2;
+					copy = buffer;
+					buf = malloc(sizeof(char) * buf_size);
+					if (buffer == NULL)
+					{
+						return (copy);
+					}
+					_memset(buffer, buf_size);
+					_strcpy(copy, buffer);
+					free(copy);
+				}
+			buffer[i] = ch;
+			ch = getchar();
+			i++;
+		}
+	if (input && i > 1024)
+		free(input);
+	return (buffer);
+}
---
+++ shell.h
@@ -0,0 +1,48 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/**
+ * struct flags - the path of the file
+ *
+ * @path: this is the path to the file
+ * @next: ptr to next file
+ *
+*/
+typedef struct flags
+{
+	char *path;
+	struct flags *next;
+} flag;
+
+size_t strlen(const char *s);
+size_t wordlen(char **s);
+int strcpy(char *dest, char *src);
+char *strtok(char *str, char **ptr, char s);
+char *check_path(char *fn);
+char *get_path(char *fn, char *h);
+char *check_cwd(char *fn);
+char *get_cwd(char *fn);
+int _memset(char *mem, size_t size);
+char *getcwd();
+flag *gethead();
+void free_pointer(char *ptr);
+void release_string_array_memory(char *argv[]);
+int check_space(char *s);
+int handle_line(char *input);
+void exit_shell(char **, char *);
+int handle_argv(char **argv, char *line, int env_set, int pre_in, int pre_st);
+int _strcmp(char *dest, char *src);
+int is_defined(char *argv1, char *argv2);
+char *_abs_path(char **argv, char *cwd_path, char *path_in);
+void free_all_memory(char *input, char **argv, char *cwd_path, char *path);
+char *_getline();
+
+#endif
